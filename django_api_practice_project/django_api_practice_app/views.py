146bc91d80de18fdce100c63b8554cd88f7444d7
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-20 16:57:23 -0400

    end of day

--- django_api_practice_project/django_api_practice_app/views.py
+++ django_api_practice_project/django_api_practice_app/views.py
@@ -1,9 +1,11 @@
 
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
 # Create your views here.
+# python views.py  
 
 
 class StockViewSet(viewsets.ModelViewSet):
@@ -15,10 +17,17 @@ class StockViewSet(viewsets.ModelViewSet):
         stock_serializer = StockSerializer(data = request.data)
         stock_serializer.is_valid(raise_exception=True)
         stock_serializer.save()
+        print('STOCK BABY!  ', stock)
+        # vehicle_stock = Vehicle.objects.get(id = stock.stock.id)
+        # order_that_i_grabbed = CustomerOrder.objects.get(id=id).first()
+        # print(order_that_i_grabbed)
+        # print(vehicle_stock)
+        # vehicle_type_from_order = order_that_i_grabbed.vehicles
+        # stock = vehicle_type_from_order.stock - order_that_i_grabbed.order_quantity 
+        return Response()
+        
 
-        # vehicle_stock = Vehicle.objects.get(id=stock.vehicle_stock.id)
-        # if (int(request.data['stock'])):
-
+#I want to update the stock of the vehicle based upon the customer order's order_quantity
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
