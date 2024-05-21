cf48c7a910085a2391432e1be40d6bcba3c9e779
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-21 11:53:58 -0400

    updated my models

--- django_api_practice_project/django_api_practice_app/models.py
+++ django_api_practice_project/django_api_practice_app/models.py
@@ -18,7 +18,7 @@ class Vehicle(models.Model):
     color = models.TextField(max_length=50, default='black', null=True)
 
     def __str__(self):
-        return f'Type: {self.type}, Price: ${self.stock}, Color: {self.color}'
+        return f'Type: {self.type}, Price: ${self.price}, Color: {self.color}'
 
 
 class Customer(models.Model):
@@ -32,8 +32,7 @@ class CustomerOrder(models.Model):
     date_day = models.PositiveIntegerField(default=12)
     date_year = models.PositiveIntegerField(default=2024)
     customer_name = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, related_name='names')
-    vehicles = models.ManyToManyField(Vehicle, related_name='order') #type, price, color
-    order_quantity = models.PositiveIntegerField(default=1)
+    # vehicles = models.ManyToManyField(Vehicle, related_name='order') #type, price, color
     paid = models.TextField(null=True, default='not paid')
 
     def __str__(self):
@@ -41,3 +40,11 @@ class CustomerOrder(models.Model):
         for v in self.vehicles.all():
             vech_string += f"{v}"
         return f'ORDER:{self.id} {self.customer_name}, Vehicles In Order: {self.order_quantity}, Order Date:{self.date_month}-{self.date_day}-{self.date_year}, Paid: {self.paid}: \n \t {vech_string} '
+    
+class VehicleInOrder(models.Model):
+    customer_order = models.ForeignKey(CustomerOrder, on_delete=models.CASCADE, related_name="vehicle")
+    vehicle_type = models.ForeignKey(Vehicle, on_delete=models.SET_NULL, null=True, related_name='vehicle_ordered')
+    quantity_of_specific_ordered_vehicle = models.SmallIntegerField(default=1)
+
+    def __str__(self):
+        return f'Ordered {self.quantity_of_specific_ordered_vehicle} {self.vehicle_type}s'
--- django_api_practice_project/django_api_practice_app/views.py
+++ django_api_practice_project/django_api_practice_app/views.py
@@ -5,7 +5,6 @@ from .models import *
 from .serializers import *
 
 # Create your views here.
-# python views.py  
 
 
 class StockViewSet(viewsets.ModelViewSet):
@@ -13,11 +12,35 @@ class StockViewSet(viewsets.ModelViewSet):
     serializer_class = StockSerializer
 
     def update(self, request, pk=None):
+        #should i be grabbing stock first? Or really should I be grabbing the customer order, and then be grabbing the vehicle type and 
+        #quantity from that and then identifying the correct stock type and then subtracting the order quantity.
+
+        #grab the order from the request data?
+        order = CustomerOrder.objects.get(pk=pk)
+        customer_order_serializer = CustomerOrderSerializer(data = request.data)
+        customer_order_serializer.is_valid(raise_exception=True)
+        customer_order_serializer.save()
+        print('ORDER BABY!', order)
+
+        #grab the vehicle type from that specific order
+        vehicle_type = order.objects.get(vehicle=CustomerOrder.vehicles)
+        print('VEHICLE TYPE BABY!', vehicle_type)
+
+        #grab the quantity from that specific order
+        quantity = order.objects.get(quantity=CustomerOrder.order_quantity)
+        print('VEHICLE ORDER QUANTITY BABY!', quantity)
+
+        
+        # this chunk worked yesterday to pull the correct stock when we were specifically trying to update the stock page, but I want
+        # to use the above data to update the stock, not just to read when someone is updating stock directly
         stock = Stock.objects.get(pk=pk)
         stock_serializer = StockSerializer(data = request.data)
         stock_serializer.is_valid(raise_exception=True)
         stock_serializer.save()
         print('STOCK BABY!  ', stock)
+
+
+
         # vehicle_stock = Vehicle.objects.get(id = stock.stock.id)
         # order_that_i_grabbed = CustomerOrder.objects.get(id=id).first()
         # print(order_that_i_grabbed)
